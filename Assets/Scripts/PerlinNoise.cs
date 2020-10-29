388f271a80655f9ca4d56c3e8815aa979fd72534
Wen Xiang <tohwenxiang@gmail.com>
2020-10-29 19:53:04 +0800

    Basic Perlin Noise Map Generator

---
+++ Assets/Scripts/PerlinNoise.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PerlinNoise
+{
+    public static float[,] GeneratePerlinNoiseMap(int width, int height, float scale)
+    {
+        float[,] perlinNoiseMap = new float[width, height];
+
+        //prevent divide by zero error by clamping lower bounds to epsilon
+        if (scale <= 0)
+        {
+            scale = Mathf.Epsilon;
+        }
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                //convert x to non integral value by dividing it by scale
+                //sampling at integral value will return the same result
+                float samplingPointX = x / scale;
+                float samplingPointY = y / scale;
+
+                float perlinNoiseValue = Mathf.PerlinNoise(samplingPointX, samplingPointY);
+                perlinNoiseMap[x, y] = perlinNoiseValue;
+            }
+        }
+
+        return perlinNoiseMap;
+    }
+}
